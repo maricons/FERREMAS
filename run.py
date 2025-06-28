01fcb063f3909ec38884ebcf08cb7fcd8539ee58
maricons <107135463+maricons@users.noreply.github.com>
2025-06-28 01:20:43 -0400

    update app.run to read PORT from environment variable, defaulting to 8000

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from flask_app.app import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # Lee el puerto de la variable de entorno PORT, o usa 8000 por defecto (para local)
+    port = int(os.environ.get("PORT", 8000))
+
+    app.run(debug=True, host="0.0.0.0", port=port)
